ca0e3bb4099dce1743f02e041fde962af9d67685
thispacket <pathetiiiiic@gmail.com>
2023-12-24 18:02:46 +0700

    feat(frontend): btns for forms

---
+++ resources/js/components/UI/form/form-button.vue
@@ -0,0 +1,36 @@
+<script setup>
+    defineProps({
+        text: {
+            type: String,
+            default: 'Button'
+        },
+        active: {
+            type: String,
+            default: "Dine In"
+        }
+    })
+</script>
+
+<template>
+    <button class="form-button" :class="active === text ? 'active-button' : ''">{{ text }}</button>
+</template>
+
+<style lang="scss">
+    .form-button {
+        border: 2px solid #393C49;
+        background: inherit;
+        padding: 7px 12px;
+        border-radius: 8px;
+        color: var(--primary-color);
+        cursor: pointer;
+        font-family: "Barlow", sans-serif;
+        font-weight: 600;
+        font-size: 14px;
+    }
+
+    .active-button {
+        background: var(--primary-color);
+        color: white;
+        border-color: var(--primary-color);
+    }
+</style>
--- resources/js/views/Home.vue
+++ resources/js/views/Home.vue
@@ -1,18 +1,27 @@
 <template>
     <div class="home-page">
-        <v-header/>
-        <menu-list/>
+        <div class="home-content">
+            <v-header/>
+            <menu-list/>
+        </div>
     </div>
+    <order-sidebar/>
 </template>
 
 <script setup>
+import OrderSidebar from "../components/UI/layout/order-sidebar.vue";
+import MenuList from "../components/UI/layout/menu-list.vue";
+import VHeader from "../components/UI/layout/v-header.vue";
+
+
 </script>
 
 <style lang="scss">
     .home-page {
         padding: 28px;
         width: 100%;
-        margin-left: calc(104px);
+        display: flex;
+
 
         .sub-title {
             margin-top: 4px;
